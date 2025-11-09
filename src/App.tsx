ebacda4f83bb4f31ddbe8bf5497f135eab82b44a
Andysword <andrewhuang2019@gmail.com>
2025-11-08 22:01:16 -0600

    fixed website errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 // src/App.tsx
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import Home from "./pages/Home";
+import Tracking from "./pages/Tracking";
+import Scheduling from "./pages/Scheduling";
 import mapUrl from "./assets/Map.svg";
 import "./index.css";
 import StatsPage from './Discrepency';
