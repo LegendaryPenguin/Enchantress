f770a2aac7d1a370568c35c2b0158861985fcaf4
Andysword <andrewhuang2019@gmail.com>
2025-11-09 05:47:15 -0600

    added forecast function

--- src/pages/Scheduling.tsx
+++ src/pages/Scheduling.tsx
@@ -1,5 +1,33 @@
 // src/pages/Scheduling.tsx
+
+import { useData } from "../backend/FetchContext"
+import getForecast from "../backend/useForecast"
+import useCauldronRates from "../backend/useCauldronRates"
+import { useState, useEffect } from "react"
+
 export default function Scheduling() {
+  const [cauldronData, setCauldronData] = useState({});
+  const { data, ticketData } = useData();
+
+  const averageRates = useCauldronRates(data); 
+
+  useEffect(() => {
+    const cauldronKeys = Object.keys(data[0].cauldron_levels);
+    console.log(cauldronKeys);
+
+    const newData: Record<string, any[]> = {};
+
+    cauldronKeys.forEach(cauldron => {
+      let cauldronRate = getForecast(cauldron)
+      newData[cauldron] = discrepancies;
+    });
+    
+    console.log("discrepancies: ", newData);
+    console.log("cauldron general data: ", data);
+    setCauldronData(newData);
+    
+  }, [data, ticketData]);
+
   return (
     <div
       style={{
