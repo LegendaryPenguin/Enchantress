7ae569d02c9a21b70e7874192011bb61bce695e1
Andysword <andrewhuang2019@gmail.com>
2025-11-08 22:36:22 -0600

    Fixed bugs for the reactivex provider

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://hackutd2025.eog.systems",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
